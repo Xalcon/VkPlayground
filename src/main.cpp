6a6d4c8a0b2a1612e2dc0e1ba2e546ccb9f4c12f
xalcon <xalcon@xalcon.net>
2018-11-27 23:51:18 +0100

    :alembic: we got a vulkan instance :boom:

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
+#include <SDL_vulkan.h>
 #include <spdlog/spdlog.h>
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include <vulkan/vulkan.hpp>
+
+VkInstance instance;
+
+void initVulkan(SDL_Window* window)
+{
+	auto log = spdlog::get("logger");
+	log->info("Initializing vulkan instance");
+
+	vk::ApplicationInfo appInfo("VkPlayground", VK_MAKE_VERSION(0, 1, 0), "unnamed", VK_MAKE_VERSION(0, 1, 0), VK_API_VERSION_1_1);
+
+	unsigned int extCount = 0;
+	if(!SDL_Vulkan_GetInstanceExtensions(window, &extCount, nullptr))
+	{
+		const auto error = SDL_GetError();
+		log->error("Error getting instance extensions: {0}", error);
+		return;
+	}
+
+	std::vector<const char*> extensions = {};
+	unsigned int totalExtensionCount = extCount + extensions.size();
+	extensions.resize(totalExtensionCount);
+
+	std::vector<const char*> layers = {};
+	const unsigned int totalLayerCount = layers.size();
+	
+	if (!SDL_Vulkan_GetInstanceExtensions(window, &totalExtensionCount, extensions.data()))
+		throw std::exception("unable to get SDL instance extensions");
+
+	for (auto& extension : extensions)
+		log->info("Requesting extension {0}", extension);
+
+	instance = vk::createInstance(vk::InstanceCreateInfo(vk::InstanceCreateFlags(), &appInfo, totalLayerCount, layers.data(), extensions.size(), extensions.data()));
+	assert(instance);
+}
 
 void setupLogging()
 {
@@ -24,7 +60,7 @@ int main(int argc, const char* argv[])
 		"hello_sdl2",
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 		800, 480,
-		SDL_WINDOW_SHOWN
+		SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN
 	);
 
 	if (window == nullptr) {
@@ -33,6 +69,9 @@ int main(int argc, const char* argv[])
 	}
 
 	log->info("SDL2 window created");
+
+	initVulkan(window);
+
 	const auto screenSurface = SDL_GetWindowSurface(window);
 	SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
 	SDL_UpdateWindowSurface(window);
