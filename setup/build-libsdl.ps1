ff20d3829b44830ed3a9be85f2f0a48523003cb7
xalcon <xalcon@xalcon.net>
2018-11-27 17:57:40 +0100

    update build SDL2 build script to fully build via cmake

--- setup/build-libsdl.ps1
+++ setup/build-libsdl.ps1
@@ -1,7 +1,27 @@
+
 $projectDir = Resolve-Path "$PSScriptRoot/.."
 $sourcePath = "$projectDir/extern/libsdl"
-$buildPath = "$projectDir/extern/libsdl/msvc2017_build"
-set-location $sourcePath
-cmake -G "Visual Studio 15 2017" -S $sourcePath -B $buildPath
+$buildPath = "$projectDir/extern/libsdl/msvc_build"
 
-& "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" "$buildPath/SDL2.vcxproj"
+Write-Host "Configuring LibSDL via cmake, this will take a while. Grab a cup of coffee, do the laundry and walk the dog. Maybe make some food as well." -ForegroundColor Green
+
+$sw = [Diagnostics.Stopwatch]::StartNew()
+cmake -G "Visual Studio 15 2017" -S $sourcePath -B "$buildPath/x86"
+cmake -G "Visual Studio 15 2017 Win64" -S $sourcePath -B "$buildPath/x64"
+
+# Replace all occurences of SDL2d with SDL2
+# > I cant figure out how to override the debug suffix through the cmake commandline for the output files
+# > so this is the workaround
+foreach($file in (Get-ChildItem -Recurse $buildPath\**\*.vcxproj))
+{
+    Write-Host "Replacing occurences of SDL2d in $file"
+    (Get-Content $file.FullName).replace('SDL2d', 'SDL2') | Set-Content $file.FullName
+}
+
+cmake --build "$buildPath/x86" --config debug
+cmake --build "$buildPath/x86" --config release
+cmake --build "$buildPath/x64" --config debug
+cmake --build "$buildPath/x64" --config release
+$sw.Stop();
+
+Write-Host "Build LibSDL in $($sw.Elapsed)" -ForegroundColor Green
